f694a0edac0ca53ae456255eda1d8ab20a3fd600
Minindu Kothalawala <it19184036@my.sliit.lk>
2022-11-07 14:04:33 +0530

    image upload API created

--- src/main/java/messagingapp/ssd/Controllers/MessageController.java
+++ src/main/java/messagingapp/ssd/Controllers/MessageController.java
@@ -1,18 +1,25 @@
 package messagingapp.ssd.Controllers;
 
 import messagingapp.ssd.Models.Message;
+import messagingapp.ssd.Services.FileService;
 import messagingapp.ssd.Services.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
 //@CrossOrigin(origins = "*")
 @RequestMapping("/message")
 public class MessageController {
+
+    @Autowired
+    private FileService fileService;
 
     @Autowired
     private final MessageService messageService;
@@ -59,6 +66,19 @@ public class MessageController {
         try {
             List<Message> messages = messageService.GetPublicMessages();
             return new ResponseEntity<>(messages, HttpStatus.OK);
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
+    @PostMapping("/upload")
+    public ResponseEntity<?> upload(@RequestParam("file") MultipartFile file){
+        try {
+//            String jwtToken = requestTokenHeader.substring(7);
+//            String username = jwtTokenUtil.getUsernameFromToken(jwtToken);
+            return new ResponseEntity<>(fileService.addFile(file), HttpStatus.OK);
         }
         catch (Exception e){
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
